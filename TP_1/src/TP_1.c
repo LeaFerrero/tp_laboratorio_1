d764da21937e1606e009bd2ae733e5fca3db23b6
Leandro Raúl Ferrero <learferrero@gmail.com>
2022-04-14 14:03:42 -0300

    agregada la carga forzada

--- TP_1/src/TP_1.c
+++ TP_1/src/TP_1.c
@@ -32,6 +32,7 @@ int main(void)
 	float precioLatam;
 	double bitCoinAerolineas;
 	double bitCoinLatam;
+	int flagCalculos;
 
 	int minimo;
 	int descuento;
@@ -43,8 +44,9 @@ int main(void)
 	aumento = 25;
 	bitcoin = 4665933.58;
 
-	flagKm = 0;
 	flagAerolineas = 0;
+	flagKm = 0;
+	flagCalculos =	0;
 	flagLatam = 0;
 	salir = 'n';
 
@@ -122,30 +124,18 @@ int main(void)
 			break;
 
 			case 3:
-				if(flagAerolineas == 1)
+				if(flagKm == 1 && flagAerolineas == 1 && flagLatam == 1 )
 				{
 					debitoAerolineas = calcularDescuento(precioAerolineas, descuento);
 					creditoAerolineas = calcularAumento(precioAerolineas, aumento);
 					bitCoinAerolineas = pasarAMoneda(precioAerolineas, bitcoin);
-					if(flagKm == 1)
-					{
-						aerolineasPorKm = dividirFlotante(precioAerolineas, km);
-					}
-				}
+					aerolineasPorKm = dividirFlotante(precioAerolineas, km);
 
-				if(flagLatam == 1)
-				{
 					debitoLatam = calcularDescuento(precioLatam, descuento);
 					creditoLatam = calcularAumento(precioLatam, aumento);
 					bitCoinLatam = pasarAMoneda(precioLatam, bitcoin);
-					if(flagKm == 1)
-					{
-						latamPorKm = dividirFlotante(precioLatam, km);
-					}
-				}
+					latamPorKm = dividirFlotante(precioLatam, km);
 
-				if(flagAerolineas == 1 && flagLatam ==1)
-				{
 					if(precioAerolineas > precioLatam)
 					{
 						diferenciaPrecio = precioAerolineas - precioLatam;
@@ -154,70 +144,62 @@ int main(void)
 					{
 						diferenciaPrecio = precioLatam - precioAerolineas;
 					}
+
+					flagCalculos =	1;
+					printf("Se estan calculando los costos.");
+				}
+				else
+				{
+					if(flagKm == 0)
+					{
+						printf("No se han ingresado Kilómetros.");
+					}
+
+					if(flagAerolineas == 0)
+					{
+						printf("No se han ingresado precios para Aerolineas.");
+					}
+
+					if(flagAerolineas == 0)
+					{
+						printf("No se han ingresado precios para Latam.");
+					}
+
 				}
  			break;
 
 			case 4:
-				if(flagKm == 1)
+				if(flagCalculos == 1)
 				{
-					printf("\nKMs ingresados: %.2f\n\n", km);
-				}
-				else
-				{
-					printf("No se han ingresado Kilómetros.\n\n");
-				}
-				if(flagAerolineas == 1)
-				{
-					printf("Precio Aerolíneas: %.2f\n", precioAerolineas);
-					printf("a) Precio con tarjeta de debito: %.2f\n", debitoAerolineas);
-					printf("b) Precio con tarjeta de credito: %.2f\n", creditoAerolineas);
-					printf("d) Precio pagando con BitCoin: %lf\n", bitCoinAerolineas);
-					if(flagKm == 1)
-					{
-						printf("e) Mostrar precio unitario: %.2f\n\n", aerolineasPorKm);
-					}
-					else
-					{
-						printf("No se han ingresado Kilómetros.\n\n");
-					}
+					printf("\nKMs ingresados: %.2f km.\n\n", km);
 
-				}
-				else
-				{
-					printf("No se ha ingresado precio de Aerolíneas.\n\n");
-				}
-				if(flagAerolineas == 1)
-				{
-					printf("Precio Latam: %.2f\n", precioLatam);
-					printf("a) Precio con tarjeta de debito: %.2f\n", debitoLatam);
-					printf("b) Precio con tarjeta de credito: %.2f\n", creditoLatam);
-					printf("d) Precio pagando con BitCoin: %lf\n", bitCoinLatam);
-					if(flagKm == 1)
-					{
-						printf("e) Mostrar precio unitario: %.2f\n\n", latamPorKm);
-					}
-					else
-					{
-						printf("No se han ingresado Kilómetros.\n\n");
-					}
-				}
-				else
-				{
-					printf("No se ha ingresado precio de Latam.\n\n");
-				}
+					printf("Precio Aerolíneas: $ %.2f\n", precioAerolineas);
+					printf("a) Precio con tarjeta de debito: $ %.2f\n", debitoAerolineas);
+					printf("b) Precio con tarjeta de credito: $ %.2f\n", creditoAerolineas);
+					printf("d) Precio pagando con BitCoin: %lf BTC\n", bitCoinAerolineas);
+					printf("e) Mostrar precio unitario: $ %.2f\n\n", aerolineasPorKm);
 
-				if(flagAerolineas == 1 && flagLatam == 1)
-				{
-					printf("La diferencia de precio es: %.2f\n\n", diferenciaPrecio);
+					printf("Precio Latam: $ %.2f\n", precioLatam);
+					printf("a) Precio con tarjeta de debito: $ %.2f\n", debitoLatam);
+					printf("b) Precio con tarjeta de credito: $ %.2f\n", creditoLatam);
+					printf("d) Precio pagando con BitCoin: $ %lf\n", bitCoinLatam);
+					printf("e) Mostrar precio unitario: $ %.2f\n\n", latamPorKm);
+
+					printf("La diferencia de precio es: $ %.2f\n\n", diferenciaPrecio);
 				}
 				else
 				{
-					printf("No se pudo calcular la diferencia de precio porque faltan valores.\n\n");
+						printf("\nError, faltan datos para hacer las operaciones.\n");
 				}
 			break;
 
 			case 5:
-				printf("Aca se harcodean lo datos\n");
+				km = 7090;
+				precioAerolineas = 162965;
+				precioLatam = 159339;
+				flagKm = 1;
+				flagAerolineas = 1;
+				flagLatam = 1;
 			break;
 
 			case 6:

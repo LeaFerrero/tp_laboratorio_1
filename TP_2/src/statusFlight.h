881b85e0e183b2e378e954fdd52286d172cbc2ef
Leandro Raúl Ferrero <learferrero@gmail.com>
2022-05-14 20:57:26 -0300

    Faltan detalles

---
+++ TP_2/src/statusFlight.h
@@ -0,0 +1,28 @@
+/*
+ * statusFlight.h
+ *
+ *  Created on: 13 may. 2022
+ *      Author: wilo waiko
+ */
+
+#ifndef STATUSFLIGHT_H_
+#define STATUSFLIGHT_H_
+
+#include <stdio.h>
+#include <string.h>
+#include "entradas.h"
+
+struct{
+
+	int idStatus;
+	char nameStatus[51];
+
+}typedef StatusFlight;
+
+void hardcodestatusFlight(StatusFlight list[], int len);
+void showstatusFlight(StatusFlight list[], int len);
+int loadstatusFlight(StatusFlight list[], int len);
+
+
+
+#endif /* STATUSFLIGHT_H_ */

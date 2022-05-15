80e985666c7864339fad312d28a462077c050681
Leandro Raúl Ferrero <learferrero@gmail.com>
2022-05-14 21:08:33 -0300

    Corregido el nombre

--- TP_[1]/src/TP_1.c
+++ TP_[1]/src/TP_1.c
@@ -8,6 +8,8 @@
  ============================================================================
  */
 
+#include <stdio.h>
+#include <stdlib.h>
 #include "entradas.h"
 #include "validaciones.h"
 #include "operaciones.h"

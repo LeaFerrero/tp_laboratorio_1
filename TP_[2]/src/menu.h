74089dd54a16595b477039d18823ca4605efe3b8
Leandro Raúl Ferrero <learferrero@gmail.com>
2022-05-14 22:30:06 -0300

    Faltan detalles

---
+++ TP_[2]/src/menu.h
@@ -0,0 +1,20 @@
+/*
+ * menu.h
+ *
+ *  Created on: 1 may. 2022
+ *      Author: wilo waiko
+ */
+
+#ifndef MENU_H_
+#define MENU_H_
+
+#include <stdio.h>
+#include <stdlib.h>
+#include "entradas.h"
+
+int mostrarMenuPrincipal();
+int mostrarMenuModificacion();
+int mostrarMenuListar();
+int mostrarMenuSN(char mensaje[]);
+
+#endif /* MENU_H_ */
